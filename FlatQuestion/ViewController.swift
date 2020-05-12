5ae4e898c1795ec3f2c065d3ce53ddf6afa9bcc0
Andrey <olesov198@gmail.com>
2020-05-13 00:06:12 +0300

    Initial Commit

---
+++ FlatQuestion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlatQuestion
+//
+//  Created by Андрей Олесов on 5/13/20.
+//  Copyright © 2020 Андрей Олесов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
