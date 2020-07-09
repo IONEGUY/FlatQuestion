ad6b8ea023471c7335ae757ae12db221b5bf2be4
Andrey <olesov198@gmail.com>
2020-07-10 01:13:50 +0300

    now collection layout depends on count of images (FlatModalVC and CreateFlatVC)

--- FlatQuestion/ViewControllers/CreateFlatViewController.swift
+++ FlatQuestion/ViewControllers/CreateFlatViewController.swift
@@ -107,6 +107,21 @@ private extension CreateFlatViewController {
     
         self.view.layoutIfNeeded()
     }
+    
+    func changeLayout() {
+        switch arrayWithImages.count {
+        case 1:
+            self.collectionView.collectionViewLayout = generateLayoutOnePhoto()
+        case 2:
+            self.collectionView.collectionViewLayout = generateLayoutTwoPhotos()
+        case 3:
+            self.collectionView.collectionViewLayout = generateLayout()
+        default:
+            self.collectionView.collectionViewLayout = generateLayout()
+        }
+        
+        self.view.layoutIfNeeded()
+    }
 
     func generateLayout() -> UICollectionViewLayout {
         let fullPhotoItem = NSCollectionLayoutItem(
@@ -161,6 +176,84 @@ private extension CreateFlatViewController {
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }
+    func generateLayoutOnePhoto() -> UICollectionViewLayout {
+        let fullPhotoItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(2/3)))
+        fullPhotoItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 2,
+            bottom: 0,
+            trailing: 2)
+        
+        let trailingGroup = NSCollectionLayoutGroup.horizontal(
+        layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)),
+        subitem: fullPhotoItem,
+        count: 1)
+        
+        let section = NSCollectionLayoutSection(group: trailingGroup)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+
+
+    func generateLayoutTwoPhotos() -> UICollectionViewLayout {
+        let fullPhotoItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(2/3)))
+        fullPhotoItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 2,
+            bottom: 0,
+            trailing: 2)
+        
+        let mainItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(2/3),
+                heightDimension: .fractionalHeight(1.0)))
+        mainItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 2,
+            leading: 2,
+            bottom: 2,
+            trailing: 2)
+        let pairItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.5)))
+        pairItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 2,
+            leading: 2,
+            bottom: 2,
+            trailing: 2)
+        let trailingGroup = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/3),
+                heightDimension: .fractionalHeight(1.0)),
+            subitem: pairItem,
+            count: 1)
+        // 1
+        let mainWithPairGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(5/10)),
+            subitems: [mainItem, trailingGroup])
+        let nestedGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)),
+            subitems: [
+                mainWithPairGroup,
+                fullPhotoItem
+            ]
+        )
+        let section = NSCollectionLayoutSection(group: nestedGroup)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
     
     func setupPickers() {
         dateTextField.inputView = datePicker
@@ -193,6 +286,7 @@ private extension CreateFlatViewController {
         } else {
             self.collectionViewHeightConstraint.constant = 191
         }
+        changeLayout()
         
         UIView.animate(withDuration: 0.6) {
             self.view.layoutIfNeeded()
--- FlatQuestion/ViewControllers/FlatModalViewController.swift
+++ FlatQuestion/ViewControllers/FlatModalViewController.swift
@@ -153,7 +153,13 @@ class FlatModalViewController: UIViewController {
     }
     
     private func setupCollectionView() {
-        self.collectionView.collectionViewLayout = generateLayout()
+        switch flat?.images?.count {
+        case 1: self.collectionView.collectionViewLayout = generateLayoutOnePhoto()
+        case 2: self.collectionView.collectionViewLayout = generateLayoutTwoPhotos()
+        case 3: self.collectionView.collectionViewLayout = generateLayout()
+        default:
+            self.collectionView.collectionViewLayout = generateLayoutOnePhoto()
+        }
         collectionView.register(UINib(nibName: FlatPhotoCollectionViewCell.identifier, bundle: nil),
         forCellWithReuseIdentifier: FlatPhotoCollectionViewCell.identifier)
         collectionView.delegate = self
@@ -216,6 +222,86 @@ class FlatModalViewController: UIViewController {
         return layout
     }
 }
+
+    func generateLayoutOnePhoto() -> UICollectionViewLayout {
+        let fullPhotoItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(2/3)))
+        fullPhotoItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 2,
+            bottom: 0,
+            trailing: 2)
+        
+        let trailingGroup = NSCollectionLayoutGroup.horizontal(
+        layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)),
+        subitem: fullPhotoItem,
+        count: 1)
+        
+        let section = NSCollectionLayoutSection(group: trailingGroup)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+
+
+    func generateLayoutTwoPhotos() -> UICollectionViewLayout {
+        let fullPhotoItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(2/3)))
+        fullPhotoItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 2,
+            bottom: 0,
+            trailing: 2)
+        
+        let mainItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(2/3),
+                heightDimension: .fractionalHeight(1.0)))
+        mainItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 2,
+            leading: 2,
+            bottom: 2,
+            trailing: 2)
+        let pairItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.5)))
+        pairItem.contentInsets = NSDirectionalEdgeInsets(
+            top: 2,
+            leading: 2,
+            bottom: 2,
+            trailing: 2)
+        let trailingGroup = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/3),
+                heightDimension: .fractionalHeight(1.0)),
+            subitem: pairItem,
+            count: 1)
+        // 1
+        let mainWithPairGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(5/10)),
+            subitems: [mainItem, trailingGroup])
+        let nestedGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)),
+            subitems: [
+                mainWithPairGroup,
+                fullPhotoItem
+            ]
+        )
+        let section = NSCollectionLayoutSection(group: nestedGroup)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+
 
 extension FlatModalViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
